24b7394767b096425d1312d62b181759b1d58828
kiran santosh upase <kvsupase@gmail.com>
2022-01-11 18:24:41 +0530

    searching in sorted array

---
+++ Array/searching/Sum_pair_SE3.py
@@ -0,0 +1,20 @@
+# Python program for searching in
+# sorted array
+# quick sort required
+
+def searchingsum(arr, l):
+	n = 0
+	for i in range(l):
+		if arr[i] + arr[i+1]
+
+def printarray(arr, l):
+	for i in range(l):
+		print(arr[i], end = " ")
+	print()
+
+
+if __name__ == "__main__":
+	arr = [0, -1, 2, -3, 1]
+	l = len(arr)
+	print("Original array : ", end = "")
+	printarray(arr, l)
